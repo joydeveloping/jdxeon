74951d1151756d780cdfeea0eafbd65cf2a3bee2
Alexey Rybakov <rybakov@ians.aero>
2015-12-08 16:25:12 +0300

    hello

--- src_f/tests/omp_hello/omp_hello.f90
+++ src_f/tests/omp_hello/omp_hello.f90
@@ -5,12 +5,38 @@ program OMP_Hello
 use omp_lib
 implicit none
 
-integer :: num_threads, max_threads
+    ! Number of threads for parallel scope.
+    print *, "Number of threads = ", OMP_Get_Num_Threads()
 
-    num_threads = OMP_Get_Num_Threads()
-    max_threads = OMP_Get_Max_Threads()
-    print *, "Number of threads = ", num_threads
-    print *, "Max..m of threads = ", max_threads
+    ! Maximum number of threads for parallel scope.
+    print *, "Max..m of threads = ", OMP_Get_Max_Threads()
+
+    ! Check dynamic threads count.
+    call OMP_Set_Dynamic(.TRUE.)
+    print *, "OMP dynamic       = ", OMP_Get_Dynamic()
+
+    ! Number of processors.
+    print *, "Number of procs.  = ", OMP_Get_Num_Procs()
+
+    ! Check nested OMP.
+    call OMP_Set_Nested(.TRUE.)
+    print *, "OMP nested        = ", OMP_Get_Nested()
+
+    ! Parallel.
+    !$OMP parallel
+    print *, "Thread number     = ", OMP_Get_Thread_Num()
+    !$OMP end parallel
+
+    ! Master.
+    !$OMP master
+    print *, "Master thread......"
+    !$OMP end master
+
+    ! Check in parallel.
+    print *, "Not parallel      : ", OMP_In_Parallel()
+    !$OMP parallel
+    print *, "    Parallel      : ", OMP_In_Parallel()
+    !$OMP end parallel
 
 end program OMP_Hello
 
